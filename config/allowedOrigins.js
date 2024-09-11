19d750eccd6aeadec286523804ea9e146b6c07d4
kinghanzz <kinghanz2000@gmail.com>
2024-09-11 10:15:58 +0200

    ready to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-      'https://technotes.onrender.com'
+      'https://technotes.onrender.com',
+      'https://technotes-api-3cvr.onrender.com'
 ]
 
 module.exports = allowedOrigins
